b22a3d3462a9a215a26fb854d62bc10e51805f05
Maciej Makowski <maciej.makowski@swmansion.com>
2024-07-10 13:47:16 +0200

    feat: added basic jsi example

--- cpp/react-native-audio-context.cpp
+++
@@ -1,7 +0,0 @@
-#include "react-native-audio-context.h"
-
-namespace audiocontext {
-	double multiply(double a, double b) {
-		return a * b;
-	}
-}
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-const AudioContext = require('./NativeAudioContext').default;
-
-export function multiply(a: number, b: number): number {
-  return AudioContext.multiply(a, b);
-}
