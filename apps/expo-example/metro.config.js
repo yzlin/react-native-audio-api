24e59c9dc5a3ad376a549c2e84b6baf44b381931
Michał Sęk <michal.sek@swmansion.com>
2024-09-27 10:28:38 +0200

    fix: expo 51 and rn 0.75 coexistence (#136)
    Co-authored-by: Maciej Makowski <120780663+maciejmakowski2003@users.noreply.github.com>

--- apps/expo-example/metro.config.js
+++ apps/expo-example/metro.config.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const { getDefaultConfig } = require('@expo/metro-config');
-
-const root = path.resolve(__dirname, '../..');
-
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -10,12 +7,5 @@ const root = path.resolve(__dirname, '../..');
  * @type {import('metro-config').MetroConfig}
  */
 const config = getDefaultConfig(__dirname);
-
-config.watchFolders = [root];
-
-config.resolver.nodeModulesPaths = [
-  path.resolve(root, 'node_modules'),
-  path.resolve(__dirname, 'node_modules'),
-];
 
 module.exports = config;
