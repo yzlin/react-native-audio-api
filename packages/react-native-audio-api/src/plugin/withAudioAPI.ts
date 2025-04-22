b3cb81c66000a619c15aee5099bb1181df156fc9
Maciej Makowski <120780663+maciejmakowski2003@users.noreply.github.com>
2025-04-22 10:54:58 +0200

    Chore/rn audio api plugin (#399)
    * chore: added some example code
    
    * feat: added audio-api plugin with config
    
    * fix: small fix
    
    * feat: trying to add extra settings for foreground service to plugin
    
    * feat: added setting fs type and parametrized plugin
    
    * fix: small fix
    
    * fix: added changes requested in PR
    
    * fix: added default values for androidFS args
    
    ---------
    
    Co-authored-by: Maciej Makowski <maciej.makowski2608@gmail.com>

---
+++ packages/react-native-audio-api/src/plugin/withAudioAPI.ts
@@ -0,0 +1,90 @@
+import {
+  AndroidConfig,
+  createRunOncePlugin,
+  ConfigPlugin,
+  withInfoPlist,
+  withAndroidManifest,
+} from '@expo/config-plugins';
+
+const pkg = require('react-native-audio-api/package.json');
+
+const withBackgroundAudio: ConfigPlugin = (config) => {
+  return withInfoPlist(config, (iosConfig) => {
+    iosConfig.modResults.UIBackgroundModes = [
+      ...Array.from(
+        new Set([...(iosConfig.modResults.UIBackgroundModes ?? []), 'audio'])
+      ),
+    ];
+
+    return iosConfig;
+  });
+};
+
+const withAndroidPermissions: ConfigPlugin<{
+  androidFSPermissions: string[];
+}> = (config, { androidFSPermissions }) => {
+  return AndroidConfig.Permissions.withPermissions(
+    config,
+    androidFSPermissions
+  );
+};
+
+const withForegroundService: ConfigPlugin<{
+  androidFSTypes: string[];
+}> = (config, { androidFSTypes }) => {
+  return withAndroidManifest(config, (mod) => {
+    const manifest = mod.modResults;
+    const mainApplication =
+      AndroidConfig.Manifest.getMainApplicationOrThrow(manifest);
+
+    const SFTypes = androidFSTypes.join('|');
+
+    const serviceElement = {
+      $: {
+        'android:name': 'system.MediaNotificationManager$NotificationService',
+        'android:stopWithTask': 'true',
+        'android:foregroundServiceType': SFTypes,
+      },
+      intentFilter: [],
+    };
+
+    if (!mainApplication.service) {
+      mainApplication.service = [];
+    }
+
+    mainApplication.service.push(serviceElement);
+
+    return mod;
+  });
+};
+
+const withAudioAPI: ConfigPlugin<{
+  iosBackgroundMode?: boolean;
+  androidForegroundService?: boolean;
+  androidFSPermissions?: string[];
+  androidFSTypes?: string[];
+}> = (
+  config,
+  {
+    iosBackgroundMode = true,
+    androidForegroundService = true,
+    androidFSPermissions = [],
+    androidFSTypes = [],
+  }
+) => {
+  if (iosBackgroundMode) {
+    config = withBackgroundAudio(config);
+  }
+  if (androidForegroundService) {
+    config = withAndroidPermissions(config, {
+      androidFSPermissions,
+    });
+    config = withForegroundService(config, {
+      androidFSTypes,
+    });
+  }
+
+  return config;
+};
+
+export default createRunOncePlugin(withAudioAPI, pkg.name, pkg.version);
