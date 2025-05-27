3f80a67f297a0c32a7b0ed0d0156347c4795049f
Maciej Makowski <120780663+maciejmakowski2003@users.noreply.github.com>
2025-05-27 11:33:28 +0200

    fix: fixed NativeAudioAPIModuleSpec on oldArch (#470)
    Co-authored-by: Maciej Makowski <maciej.makowski2608@gmail.com>

--- packages/react-native-audio-api/android/src/oldarch/NativeAudioAPIModuleSpec.java
+++ packages/react-native-audio-api/android/src/oldarch/NativeAudioAPIModuleSpec.java
@@ -10,67 +10,71 @@
  * @nolint
  */
 
-package com.swmansion.audioapi;
+ package com.swmansion.audioapi;
 
-import com.facebook.proguard.annotations.DoNotStrip;
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.turbomodule.core.interfaces.TurboModule;
-import javax.annotation.Nonnull;
+ import com.facebook.proguard.annotations.DoNotStrip;
+ import com.facebook.react.bridge.Promise;
+ import com.facebook.react.bridge.ReactApplicationContext;
+ import com.facebook.react.bridge.ReactContextBaseJavaModule;
+ import com.facebook.react.bridge.ReactMethod;
+ import com.facebook.react.bridge.ReadableArray;
+ import com.facebook.react.bridge.ReadableMap;
+ import com.facebook.react.turbomodule.core.interfaces.TurboModule;
+ import javax.annotation.Nonnull;
 
-public abstract class NativeAudioAPIModuleSpec extends ReactContextBaseJavaModule implements TurboModule {
-  public static final String NAME = "AudioAPIModule";
+ public abstract class NativeAudioAPIModuleSpec extends ReactContextBaseJavaModule implements TurboModule {
+   public static final String NAME = "AudioAPIModule";
 
-  public NativeAudioAPIModuleSpec(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+   public NativeAudioAPIModuleSpec(ReactApplicationContext reactContext) {
+     super(reactContext);
+   }
 
-  @Override
-  public @Nonnull String getName() {
-    return NAME;
-  }
+   @Override
+   public @Nonnull String getName() {
+     return NAME;
+   }
 
-  @ReactMethod(isBlockingSynchronousMethod = true)
-  @DoNotStrip
-  public abstract boolean install();
+   @ReactMethod(isBlockingSynchronousMethod = true)
+   @DoNotStrip
+   public abstract boolean install();
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void setLockScreenInfo(ReadableMap info);
+   @ReactMethod(isBlockingSynchronousMethod = true)
+   @DoNotStrip
+   public abstract double getDevicePreferredSampleRate();
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void resetLockScreenInfo();
+   @ReactMethod
+   @DoNotStrip
+   public abstract void setAudioSessionActivity(boolean enabled, Promise promise);
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void enableRemoteCommand(String name, boolean enabled);
+   @ReactMethod
+   @DoNotStrip
+   public abstract void setAudioSessionOptions(String category, String mode, ReadableArray options);
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void setAudioSessionOptions(String category, String mode, ReadableArray options);
+   @ReactMethod
+   @DoNotStrip
+   public abstract void setLockScreenInfo(ReadableMap info);
 
-  @ReactMethod(isBlockingSynchronousMethod = true)
-  @DoNotStrip
-  public abstract double getDevicePreferredSampleRate();
+   @ReactMethod
+   @DoNotStrip
+   public abstract void resetLockScreenInfo();
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void observeAudioInterruptions(boolean enabled);
+   @ReactMethod
+   @DoNotStrip
+   public abstract void enableRemoteCommand(String name, boolean enabled);
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void observeVolumeChanges(boolean enabled);
+   @ReactMethod
+   @DoNotStrip
+   public abstract void observeAudioInterruptions(boolean enabled);
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void requestRecordingPermissions(Promise promise);
+   @ReactMethod
+   @DoNotStrip
+   public abstract void observeVolumeChanges(boolean enabled);
 
-  @ReactMethod
-  @DoNotStrip
-  public abstract void checkRecordingPermissions(Promise promise);
-}
+   @ReactMethod
+   @DoNotStrip
+   public abstract void requestRecordingPermissions(Promise promise);
+
+   @ReactMethod
+   @DoNotStrip
+   public abstract void checkRecordingPermissions(Promise promise);
+ }
